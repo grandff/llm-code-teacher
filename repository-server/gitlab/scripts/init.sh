a9735c8db1e1f6022c92f58bea5cdcc934549a15
Administrator <root@example.com>
2024-09-02 16:21:32 +0900

    Fix the gitlab auto rebote config

--- repository-server/gitlab/scripts/init.sh
+++ repository-server/gitlab/scripts/init.sh
@@ -6,11 +6,17 @@ LOG_FILE="/var/log/gitlab_setup.log"
 # 로그 파일에 출력 저장
 exec > >(tee -a ${LOG_FILE} ) 2>&1
 
+
+# GitLab의 runit 서비스 디렉토리 시작 (백그라운드에서 실행)
+echo "Starting runit service directory..."
+/opt/gitlab/embedded/bin/runsvdir-start &
+
+# runit 서비스가 안정적으로 시작될 시간을 확보하기 위해 잠시 대기
+sleep 5
+
 # GitLab 서비스 초기화
 gitlab-ctl reconfigure
 
-# GitLab 서비스 시작
-gitlab-ctl start
 
 # 관리자 비밀번호 설정
 echo "Setting GitLab root password..."
@@ -39,6 +45,7 @@ EOF
 
 # GitLab 서비스 재시작
 gitlab-ctl restart
+
 
 # 컨테이너를 포그라운드에서 유지
 tail -f /dev/null
